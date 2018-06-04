ddcfe703dee7054e5be00f77e90b479301e2cc5a
Alex Bunnell <thacheshirecat@gmail.com>
2018-06-04 13:02:41 -0700

    Add a new array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
-$(document).ready(function(){
+var things = ["thing-one", "thing-two", "thing-three"];
 
+$(document).ready(function(){
   $("#favorite-form").submit(function(){
     event.preventDefault();
 
-    var things = ["thing-one", "thing-two", "thing-three"];
+    var newArray = [];
+    var newArrayTwo = [];
 
     things.forEach(function(thing){
       var userInput = $("input#" + thing).val();
+      newArray.push(userInput);
       $("." + thing).text(userInput);
     });
+    $(".new-array").text(newArray);
+    newArrayTwo.push(newArray[0]);
+    newArrayTwo.push(newArray[2]);
+    alert(newArrayTwo);
+    $(".one-list-two").text(newArrayTwo[0]);
+    $(".two-list-two").text(newArrayTwo[1]);
     $("#thing-list").show();
   });
-
 });
